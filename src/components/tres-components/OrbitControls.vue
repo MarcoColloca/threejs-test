fdf56df7479063254a067e97b450904dab7a4f29
MarcoColloca <marco.colloca3511@gmail.com>
2024-07-28 16:35:21 +0200

    Add → added OrbitControl Component

---
+++ src/components/tres-components/OrbitControls.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { extend, useTresContext } from '@tresjs/core'
+import { OrbitControls } from 'three/addons/controls/OrbitControls'
+
+const { camera, renderer } = useTresContext()
+
+extend({OrbitControls})
+
+</script>
+
+
+<template>
+    <TresOrbitControls
+     v-if="renderer"
+     :args="[camera, renderer?.domElement]"
+    />
+
+</template>
