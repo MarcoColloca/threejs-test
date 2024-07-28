0d0848a063d613de80b70e1537f7c06874f4cb53
MarcoColloca <marco.colloca3511@gmail.com>
2024-07-28 20:48:06 +0200

    Add → installed cientos and Added Horse 3D Model

---
+++ src/components/tres-components/Horse.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useAnimations, useGLTF } from '@tresjs/cientos'
+
+
+const { scene: model, animations, ...rest } = await useGLTF(
+  '/models/Test/Horse.gltf',
+)
+
+const { actions, mixer } = useAnimations(animations, model)
+//const currentAction = ref(actions.Greeting)
+//currentAction.value.play()
+</script>
+
+<template>
+  <primitive :object="model" :scale="[0.005, 0.005, 0.005]"/>
+</template>
